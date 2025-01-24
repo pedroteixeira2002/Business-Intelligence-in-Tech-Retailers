39e44d21f2796d26e0bdd66486e8634c01cfe144
pedroteixeira2002 <8200489@estg.ipp.pt>
2025-01-24 12:53:55 +0000

    Revert "frontend add"
    This reverts commit 3db8723eecee76b6a48f76a313af2d5f8d5d15c9.

--- frontend/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { TestsComponent } from './tests/tests.component';
-
-export const routes: Routes = [
-    { path: 'tests', component: TestsComponent }
-];
